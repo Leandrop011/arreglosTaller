aea5b8b4d746a34069d1801b16f395d203225bce
Leandro <jordanspozo@gmail.com>
2024-11-13 06:19:01 -0500

    inicio

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "funciones.h"
 int main () {
-
+    int num[10], i;
+    printf("Por favor Ingrese los 10 Numeros: ");
+    for ( i = 0; i < 10; i++)
+    {
+        printf("\n");
+        scanf("%i", &num[i]);
+    }
+    
     return 0;
 }
